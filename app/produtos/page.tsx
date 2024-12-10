6a81ac6c98e0ff3dd247d88a27e95eb354beeab9
VicenteGama22306126 <160889051+vicentepgama@users.noreply.github.com>
2024-12-10 16:31:58 +0000

    fim

--- app/produtos/page.tsx
+++ app/produtos/page.tsx
@@ -8,8 +8,8 @@ import { Product } from "@/app/models/interfaces";
 export default function ProdutosPage() {
   return (
     <div>
-      <h1 className="text-2xl font-bold text-center my-6">Produtos Disponíveis</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 px-4">
+      <h1 >Produtos Disponíveis</h1>
+      <div>
         {JSON.parse(JSON.stringify(produtos)).map((produto: Product) => (
           <Card key={produto.id} product={produto} />
         ))}
