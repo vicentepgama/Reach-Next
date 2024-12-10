cc5e2ee66a9ba21ab4a8cd8105d0c882d5149cb5
VicenteGama22306126 <160889051+vicentepgama@users.noreply.github.com>
2024-12-10 15:20:28 +0000

    imagens tecnologias

--- app/produtos/page.tsx
+++ app/produtos/page.tsx
@@ -1,25 +1,19 @@
 'use client';
 
 import React from 'react';
-import useSWR from 'swr';
-import { Product } from "@/app/models/interfaces";
+import Card from "@/componentes/Card/Card";
+import produtos from "@/app/data/produtos.json";
+import { Product } from '../models/interfaces';
 
-const fetcher = (url: string) => fetch(url).then((res) => res.json());
-
-export default function Page() {
-  const { data, error, isLoading } = useSWR<Product[]>('/api/products', fetcher);
-
-  if (isLoading) return <div>A carregar os produtos...</div>;
-  if (error) return <div>Erro ao carregar os produtos.</div>;
-
+export default function ProdutosPage() {
   return (
     <div>
-      <h1>Lista de Produtos</h1>
-      <ul>
-        {data?.map((product) => (
-          <li key={product.id}>{product.nome}</li> 
+      <h1 className="text-2xl font-bold text-center my-6">Produtos Disponíveis</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 px-4">
+        {JSON.parse(JSON.stringify(produtos)).map((produto: Product) => (
+          <Card key={produto.id} product={produto} />
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
