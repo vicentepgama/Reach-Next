2593c4f36a4a806f89b50855c60df21e2bb5a50b
VicenteGama22306126 <160889051+vicentepgama@users.noreply.github.com>
2024-12-12 11:12:30 +0000

    correcao

--- componentes/Card/Card.js
+++ componentes/Card/Card.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './Card.module.css'; 
 
-const Card = ({ produto, adicionarAoCesto, removerDoCesto,Tecnologia }) => (
+const Card = ({ produto, adicionarAoCesto, removerDoCesto }) => (
   <article className={styles['grid-item']}>
     <header>
       <h2 className={styles['title-product']}>{produto.title}</h2>
